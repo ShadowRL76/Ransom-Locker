1b89ec24bc664436ec3c5ea3e006f5dd0ab6bff8
ShadowRL76 <rylankass@gmail.com>
2024-12-18 07:19:16 -0500

    Add WinMain for wxWidgets with custom main function
    Introduce a `WinMain` function within an `extern "C"` block to ensure C linkage, enabling wxWidgets to work with a custom main function. Add a comment explaining the necessity of this code.

--- Ransom Locker/main.cpp
+++ Ransom Locker/main.cpp
@@ -2,6 +2,8 @@
 
 IMPLEMENT_APP_NO_MAIN(RansomLockerApp) 
 
+
+//Note: This is needed for the wxWidgets library to work with a custom Main Function!
 extern "C" int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, wxCmdLineArgType lpCmdLine, int nCmdShow) {
     return wxEntry(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 }
